8eabec367b31322261435ef0fbf7906a718725ef
B.C. van Duuren <boy@vanduuren.xyz>
2016-12-15 22:02:12 +0100

    Initial work on main and payload handler

---
+++ github2docker.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"./lib"
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/viper"
+	"net/http"
+	"strings"
+)
+
+const (
+	ENVPREFIX          = "githubrunner"
+	CONFIGFILE         = "config"
+	CONFIGPATH         = "."
+	LOGLEVEL           = "loglevel"
+	LOGLEVEL_DEFAULT   = "info"
+	WEB_SERVER         = "web.server"
+	WEB_SERVER_DEFAULT = "127.0.0.1"
+	WEB_PORT           = "web.port"
+	WEB_PORT_DEFAULT   = "8080"
+	WEB_PATH           = "web.path"
+	WEB_PATH_DEFAULT   = "payload"
+)
+
+// Main entry point for our program. Used to read and set the configuration we'll be using.
+func main() {
+	LOGMAP := map[string]log.Level{
+		"debug": log.DebugLevel,
+		"error": log.ErrorLevel,
+		"fatal": log.FatalLevel,
+		"info":  log.InfoLevel,
+	}
+
+	log.Info("Starting githubrunner")
+
+	// Configure viper to read config from the environment
+	// We'll use a EnvKeyReplacer so GITHUBRUNNER_GIT_APIKEY
+	// overrides git.apikey defined in a config file
+	viper.SetEnvPrefix(ENVPREFIX)
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	// A config file might exist in the same dir as the githubrunner binary, but is not required
+	viper.SetConfigName(CONFIGFILE)
+	viper.AddConfigPath(CONFIGPATH)
+	viper.ReadInConfig()
+
+	// Set some defaults
+	viper.SetDefault(LOGLEVEL, LOGLEVEL_DEFAULT)
+	viper.SetDefault(WEB_SERVER, WEB_SERVER_DEFAULT)
+	viper.SetDefault(WEB_PORT, WEB_PORT_DEFAULT)
+	viper.SetDefault(WEB_PATH, WEB_PATH_DEFAULT)
+
+	// Set log level
+	logLevel := strings.ToLower(viper.GetString(LOGLEVEL))
+	if val, exists := LOGMAP[logLevel]; exists {
+		log.Info("Setting log level to " + logLevel)
+		log.SetLevel(val)
+	} else {
+		log.Error("Loglevel was set to invalid value " + logLevel + ", defaulting to " + LOGLEVEL_DEFAULT)
+	}
+
+	// Get the webserver configuration
+	webServer := viper.GetString(WEB_SERVER)
+	webPort := viper.GetString(WEB_PORT)
+	webPath := viper.GetString(WEB_PATH)
+
+	listener := webServer + ":" + webPort
+	log.Info("Opening listener on " + listener + ", handling requests at /" + webPath)
+	http.HandleFunc("/"+webPath, git.HandleWebhook)
+	err := http.ListenAndServe(listener, nil)
+	if err != nil {
+		log.Fatal("Error while opening listener: ", err)
+	}
+}
---
+++ lib/git.go
@@ -0,0 +1,58 @@
+package git
+
+import (
+	"encoding/json"
+	log "github.com/Sirupsen/logrus"
+	"net/http"
+)
+
+const EVENTHEADER = "X-GitHub-Event"
+
+type hookPayload struct {
+	Username, Password string
+}
+
+// HandleWebhook is called when we receive a request on our listener and is responsible
+// for decoding the payload and passing it to the appropriate handler for that particular event.
+// If the received event is not supported we log an error and return without doing anything.
+func HandleWebhook(w http.ResponseWriter, r *http.Request) {
+	// Map Github webhook events to functions that handle them
+	supportedEvents := map[string]func(hookPayload){
+		"push": handlePush,
+	}
+
+	log.Info("Received request on listener")
+	log.Debug("Request from " + r.UserAgent() + " at " + r.RemoteAddr)
+
+	// Check which event we received and assign the appropriate handler to eventHandler
+	var eventHandler func(hookPayload)
+	event := r.Header.Get(EVENTHEADER)
+	if event == "" {
+		log.Error("Header \"" + EVENTHEADER + "\" not set, returning")
+		return
+	} else if val, exists := supportedEvents[event]; exists {
+		eventHandler = val
+		log.Debug("Found appropriate handler for " + event + "\" event")
+	} else {
+		log.Error("Received \"" + EVENTHEADER + "\", but found no supporting handler, returning")
+		return
+	}
+
+	// Try to decode the payload
+	jsonDecoder := json.NewDecoder(r.Body)
+	var payload hookPayload
+	err := jsonDecoder.Decode(&payload)
+	if err != nil {
+		log.Error("Error while decoding payload: ", err)
+		return
+	}
+
+	log.Debug("Decoded payload to ", payload)
+	eventHandler(payload)
+}
+
+// Handle a push event to a Github repository. We will need to look at the settings for githubrunner
+// in this repository and take action accordingly.
+func handlePush(payload hookPayload) {
+	// do something
+}
