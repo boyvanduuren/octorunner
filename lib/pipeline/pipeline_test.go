e1c1c21253c60c73c3cb91b2b25ac6feca59f6d6
B.C. van Duuren <boy@vanduuren.xyz>
2017-01-30 18:57:13 +0100

    Added simple test for pipeline.ParseConfig

--- lib/pipeline/pipeline_test.go
+++ lib/pipeline/pipeline_test.go
@@ -1,9 +1,10 @@
-package pipeline
+package pipeline_test
 
 import (
 	"testing"
 	"github.com/docker/docker/pkg/testutil/assert"
 	"fmt"
+	"github.com/boyvanduuren/octorunner/lib/pipeline"
 )
 
 const foo = `
@@ -13,22 +14,41 @@ jobs:
   -
     script:
       - foo
-    stage: build
+      - bar
     allow_failure: true
   -
     script:
       - do_test
-    stage: test
+    image: test/image:latest
     allow_failure: false
 `
 
 func TestConfigParsing(t *testing.T) {
-	config, err := ParseConfig([]byte(foo))
+	config, err := pipeline.ParseConfig([]byte(foo))
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		t.Fail()
 	}
 
-	fmt.Printf("%+v", config)
+	// we should have two jobs
 	assert.Equal(t, len(config.Jobs), 2)
+
+	job1 := config.Jobs[0]
+	// the first job should have two commands in the script
+	assert.Equal(t, len(job1.Script), 2)
+	// and they should be "foo" and "bar"
+	assert.Equal(t, job1.Script[0], "foo");
+	assert.Equal(t, job1.Script[1], "bar");
+	// allow_failure should be true
+	assert.Equal(t, job1.AllowFailure, true)
+	// image should be empty
+	assert.Equal(t, job1.Image, "")
+
+	job2 := config.Jobs[1]
+	// the second job should only have one command
+	assert.Equal(t, len(job2.Script), 1)
+	// it should equal "do_test"
+	assert.Equal(t, job2.Script[0], "do_test")
+	// image should be "test/image:latest"
+	assert.Equal(t, job2.Image, "test/image:latest")
 }
