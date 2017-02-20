705e8bdf2475ad4fd4b62ee724a82981f84eec08
B.C. van Duuren <boy@vanduuren.xyz>
2017-02-20 20:31:15 +0100

    Added comments to common.go

--- lib/common/common.go
+++ lib/common/common.go
@@ -9,11 +9,19 @@ import (
 	"path/filepath"
 )
 
+/*
+CheckDirNotExists returns true if a given path doesn't exist, or is not a directory.
+ */
 func CheckDirNotExists(dir string) bool {
 	s, err := os.Stat(dir)
 	return os.IsNotExist(err) == true || !s.IsDir()
 }
 
+/*
+CreateTarball takes a source and destination. A string will be returned denoting the path the tarball should
+be copied to for the target container. The first ReadCloser is opened on the source path, the second represents
+the tarball in memory.
+ */
 func CreateTarball(source string, destination string) (string, io.ReadCloser, io.ReadCloser, error) {
 	if CheckDirNotExists(source) {
 		return "", nil, nil, fmt.Errorf("%q does not exist", source)
